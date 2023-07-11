ae6ada2231759fb9b1f7963ddfe8851ff9b6e98a
RUEI TING HUNG <101823195+erichung09060@users.noreply.github.com>
2023-07-12 03:05:03 +0800

    Update baseball.py

--- baseball.py
+++ baseball.py
@@ -12,18 +12,21 @@ def crawler(url):
             continue
         title = article.find(class_="title").text.strip()
         author =  article.find(class_="author").text.strip()
-        link =  article.find(class_="title").a['href']
+        link =  "https://www.ptt.cc" + article.find(class_="title").a['href']
 
         results.append({"title":title, "author":author, "limk":link})
     
+url = 'https://www.ptt.cc/bbs/Baseball/index3.html'
 
+while True:
+    crawler(url)
 
-url = 'https://www.ptt.cc/bbs/Baseball/index13816.html'
-crawler(url)
-
-soup = BeautifulSoup(requests.get(url).text, "html.parser")
-next_url = soup.find_all(class_="btn wide")[1]
-print(next_url.a['href'])
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    res = soup.find_all(class_="btn wide")
+    if len(res) < 4:
+        break
+    next_url = "https://www.ptt.cc" + res[1]['href']
+    url=next_url
 
 with open('result.json', 'w', encoding='utf-8') as f:
     json.dump(results, f, indent=2, sort_keys=True, ensure_ascii=False)
